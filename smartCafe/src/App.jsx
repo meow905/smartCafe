a7e68c087fbc0550846c28cc62212afbe3fe6ca5
meow905 <tabrezbunny@gmail.com>
2025-05-28 16:55:53 +0500

    smartCafe

---
+++ smartCafe/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SmartCafeCard from "./SmartCafeCard";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <SmartCafeCard />
+    </div>
+  );
+}
+
+export default App;
